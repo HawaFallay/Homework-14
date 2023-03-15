5153f8640408e5e13d28dba585953c0443960a08
HawaFallay <113000340+HawaFallay@users.noreply.github.com>
2023-03-15 12:29:56 -0700

    connection updated ,port missing

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+const Sequelize = require("sequelize");
+
+require('dotenv').config();
+let sequelize;
+
+if (process.env.CLEARDB_DATABASE_URL) {
+    Sequelize + new Sequelize(process.env.CLEARDB_DATABASE_URL)
+} else {
+    sequelize = new Sequelize(
+        process.env.DB_DB,
+        process.env.DB_USER,
+        process.env.DB_PASS,
+        {
+            host: process.env.DB_HOST,
+            dialect: 'mysql',
+            port: ,
+        },
+    );
+}
+
+module.exports = sequelize;
