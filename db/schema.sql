b9b8a920abf4ae8cc2cc5f07e0c31c5237750f6e
HawaFallay <113000340+HawaFallay@users.noreply.github.com>
2023-03-15 12:21:02 -0700

    serverjs complete and setup

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF Exists techBlog_db;
+
+CREATE DATABASE techBlog_db;
--- server.js
+++ server.js
@@ -2,7 +2,30 @@ require("dotenv").config();
 const express = require("express");
 const cookieParser = require('cookie-parser');
 
-const { engine } require('express-handlebars');
+const { engine } = require('express-handlebars');
 const path = require('path');
 
-const sequelize = require('./config/connection')
+const sequelize = require('./config/connection');
+
+const mainRouter = require(".controllers");
+const { sequelize } = require("./sequelize");
+const helpers = require('.utils/helpers');
+
+const PORT = process.env.PORT || 3001;
+
+app.engine('handlebars', engine({ helpers }));
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(mainRouter);
+app.use(express.static(path.join(_dirname, 'public')));
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT,() => {
+        console.log("Listening on: http://localhost:" + PORT);
+        
+    });
+});
