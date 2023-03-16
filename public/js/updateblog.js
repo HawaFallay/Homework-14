8b45dcd401f47bf0f0cb91180129a11f9d3a4a9e
HawaFallay <113000340+HawaFallay@users.noreply.github.com>
2023-03-16 00:51:58 -0700

    all publc js complete

--- public/js/updateblog.js
+++ public/js/updateblog.js
@@ -1,1 +1,49 @@
-document.getElementById("update-btn")
+document.getElementById("update-btn").addEventListener("click", (event) => {
+    event.preventDefault();
+    console.log("works");
+
+    const body = document.getElementById("blog-content").value;
+    const title = document.getElementById("blog-title").value;
+    const blog_id = window.location.toString().split('/')[window.location.toString().split('/').length -1];
+    console.log(body, blog_id);
+
+    fetch(`/api/blogpost/${blog_id}`, {
+        method: 'PUT',
+        headers: {
+            'contene-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            title,
+            body,
+        }),
+    }).then((result) => {
+        if (!result.ok) {
+            document.getElementById("update-error").textContent("failed to update blog");
+        } else{
+
+            document.location.replace('/dashboard');
+        }
+    });
+
+});
+
+document.getElementById("delete-btn").addEventListener("click", (event) =>{
+    event.preventDefault();
+    console.log("works");
+
+    const blog_id = window.location.toString().split('/')[window.location.toString().split('/').length -1];
+    console.log(blog_id);
+
+    fetch(`/api/blogpost/${blog_id}`, {
+        method: 'DELETE',
+        headers: {
+            'content-Type': 'application/json'
+        },
+    }).then((result) => {
+        if (!result.ok) {
+            document.getElementById("delete-error").textContent("failed to delete");
+        } else {
+            document.location.replace('/dashboard');
+        }
+    });
+});
